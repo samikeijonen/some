2f32e54a5b74a9b00967f7017501c19eb6a3807f
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-04-10 15:06:45 +0300

    Style 404 page.

--- 404.php
+++ 404.php
@@ -18,43 +18,50 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'some' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( some_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'some' ); ?></h2>
-						<ul>
+					<div class="grid-wrapper">
+						
+						<div class="404-image">
+							<p><?php echo some_get_svg( array( 'icon' => '404' ) ); ?></p>
+						</div>
+						<div class="404-search">
+							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'some' ); ?></p>
+							<?php get_search_form(); ?>
+						</div>
 						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
+
+							the_widget( 'WP_Widget_Recent_Posts' );
+
+							// Only show the widget if site has multiple categories.
+							if ( some_categorized_blog() ) :
 						?>
-						</ul>
-					</div><!-- .widget -->
 
-					<?php
-						endif;
+						<div class="widget widget_categories">
+							<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'some' ); ?></h2>
+							<ul>
+							<?php
+								wp_list_categories( array(
+									'orderby'    => 'count',
+									'order'      => 'DESC',
+									'show_count' => 1,
+									'title_li'   => '',
+									'number'     => 10,
+								) );
+							?>
+							</ul>
+						</div><!-- .widget -->
 
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'some' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+						<?php
+							endif;
 
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+							/* translators: %1$s: smiley */
+							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'some' ), convert_smilies( ':)' ) ) . '</p>';
+							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
+							the_widget( 'WP_Widget_Tag_Cloud' );
+						?>
+					</div><!-- .grid-wrapper -->
 				</div><!-- .page-content -->
+				
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->

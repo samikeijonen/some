e8c179445c66c80be6be820a5827cd1e58366933
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-05-05 21:37:07 +0300

    Change custom header arguments.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -23,54 +23,60 @@
 function some_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'some_custom_header_args', array(
 		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'default-text-color'     => 'ffffff',
+		'width'                  => 1920,
+		'height'                 => 600,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'some_header_style',
 	) ) );
 }
-add_action( 'after_setup_theme', 'some_custom_header_setup' );
+add_action( 'after_setup_theme', 'some_custom_header_setup', 15 );
 
-if ( ! function_exists( 'some_header_style' ) ) :
 /**
  * Styles the header image and text displayed on the blog.
  *
  * @see some_custom_header_setup().
  */
 function some_header_style() {
-	$header_text_color = get_header_textcolor();
-
-	/*
-	 * If no custom options for text are set, let's bail.
-	 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: HEADER_TEXTCOLOR.
-	 */
-	if ( HEADER_TEXTCOLOR === $header_text_color ) {
-		return;
+	
+	// Header text color.
+	$header_color = get_header_textcolor();
+	
+	// Header image.
+	$header_image = esc_url( get_header_image() );
+	
+	// Start header styles.
+	$style = '';
+	
+	// Header image height.
+	$header_height = get_custom_header()->height;
+	
+	// Header image width.
+	$header_width = get_custom_header()->width;
+	
+	// When to show header image.
+	$min_width = absint( apply_filters( 'some_header_bg_show', 1 ) );
+	
+	// Background arguments.
+	$background_arguments = esc_attr( apply_filters( 'some_header_bg_arguments', 'no-repeat 50% 50%' ) );
+	
+	if ( ! empty( $header_image ) ) {
+		$style .= "@media screen and (min-width: {$min_width}em) { body.custom-header-image .site-header { background-image: url({$header_image}) } }";
+	}
+	
+	/* Site title styles. */
+	if ( display_header_text() ) {
+		$style .= ".site-title, .site-title a, .site-description, .site-description a { color: #{$header_color} }";
+		$style .= ".site-title a { border-color: #{$header_color} }";
+	}
+	
+	if ( ! display_header_text() ) {
+		$style .= ".site-title, .site-description { clip: rect(1px, 1px, 1px, 1px); position: absolute; }";	
+	}
+	
+	/* Echo styles if it's not empty. */
+	if ( ! empty( $style ) ) {
+		echo "\n" . '<style type="text/css" id="custom-header-css">' . trim( $style ) . '</style>' . "\n";
 	}
 
-	// If we get this far, we have custom styles. Let's do this.
-	?>
-	<style type="text/css">
-	<?php
-		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-	?>
-		.site-title,
-		.site-description {
-			position: absolute;
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text use that.
-		else :
-	?>
-		.site-title a,
-		.site-description {
-			color: #<?php echo esc_attr( $header_text_color ); ?>;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
 }
-endif;

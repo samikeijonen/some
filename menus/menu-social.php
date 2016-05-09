759fb25150409d5312fabe8c4e3ddd97762bfe45
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-05-09 09:20:07 +0300

    Remove extra title.

--- menus/menu-social.php
+++ menus/menu-social.php
@@ -9,7 +9,6 @@
 <?php if ( has_nav_menu( 'social' ) ) : // Check if there's a menu assigned to the 'social' location. ?>
 	
 	<nav class="menu-social social-navigation menu" role="navigation" aria-label="<?php esc_attr_e( 'Social Menu', 'some' ); ?>">
-		<h2 class="screen-reader-text"><?php esc_attr_e( 'Social Menu', 'some' ); ?></h2>
 		
 		<?php wp_nav_menu(
 			array(

ee48dd6d771a050d59f065a55b75a70f622306a4
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-12-30 18:41:38 +0200

    Selective refresh for blog name and description.

--- inc/customizer.php
+++ inc/customizer.php
@@ -74,8 +74,42 @@ function some_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	
+	// Selective refresh for blog name and description.
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'some_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'some_customize_partial_blogdescription',
+		) );
+	}
 }
 add_action( 'customize_register', 'some_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @since 1.1.0
+ *
+ * @return void
+ */
+function some_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @since 1.1.0
+ *
+ * @return void
+ */
+function some_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
 
 /**
  * Sanitizes the footer content on the customize screen. Users with the 'unfiltered_html' cap can post 
